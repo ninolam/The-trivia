b8c9a125716b5165e7300ec524620196f75957dd
david <sephi@DESKTOP-OCBDTI6.localdomain>
2018-11-20 17:17:34 +0100

    plug value to parent

--- src/views/Category/CategoryContainer.js
+++ src/views/Category/CategoryContainer.js
@@ -1,23 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import api from '../../helpers/api';
 import Category from './Category';
 
 class CategoryContainer extends Component {
   state = {
     category: null,
+    currentQuestion: 0,
   }
+  
+  // createRef in order to bring back input value to its parent
+  answerInput = createRef();
+
+  // async needed when using promise
   async componentDidMount() {
     const data = await api.getCategoryById(this.props.match.params.id);
+    // stored response in the state;
     this.setState({
       category: data,
     });
   }
+
+  handleSubmit = (e) => {
+    // here I prevent de fault bh of submitting form
+    e.preventDefault();
+    const answer = this.answerInput.current.value;
+
+  }
+
   render() {
-    console.log(this.state);
+    const { category, currentQuestion } = this.state;
+    // at first render, category will be null so we need to wait
+    // before using data.
+    if (!category) return <div>is loading</div>
+
     return (
       <Category
-        categoryName={this.props.match.params.id}
-        category={this.state.category}
+        category={category}
+        currentQuestionIndex={currentQuestion}
+        handleSubmit={this.handleSubmit}
+        answerInput={this.answerInput} // plug createRef to chidlren
       />
     );
   }
