0f67f01778732b3a5b2248401ef2fac7aab424f3
ninolam <lamoureux.nino@gmail.com>
2018-12-15 11:24:49 +0100

    first commit

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Category = ({ category, currentQuestionIndex, handleSubmit, answerInput }) => {
+const Category = ({ category, currentQuestionIndex, handleSubmit, answerInput, handleScore, score, lives }) => {
   const currentQuestion = category.clues[currentQuestionIndex];
   return (
     <section>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={ handleSubmit}>
+      {/* currentQuestionIndex === category.clues_count -1 ? handleScore : */}
         <h1>You choosed: {category.title}</h1>
         <div className="question">
           <h3 className="question__title">
@@ -19,6 +20,12 @@ const Category = ({ category, currentQuestionIndex, handleSubmit, answerInput })
             Next
           </button>
         </div>
+        <div className="score_value">
+        score is:{score}
+        </div>
+        <div className="live_value">
+        lives is:{lives}
+        </div>
       </form>
     </section>
   );
@@ -27,7 +34,10 @@ const Category = ({ category, currentQuestionIndex, handleSubmit, answerInput })
 Category.propTypes = {
   category: PropTypes.shape({}).isRequired,
   currentQuestionIndex: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+  lives: PropTypes.number.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  handleScore: PropTypes.func.isRequired,
   answerInput: PropTypes.shape({
     value: PropTypes.instanceOf(HTMLInputElement)
   }),
--- src/views/Category/CategoryContainer.js
+++ src/views/Category/CategoryContainer.js
@@ -6,6 +6,8 @@ class CategoryContainer extends Component {
   state = {
     category: null,
     currentQuestion: 0,
+    score: 0,
+    lives: 3
   }
 
   // createRef in order to bring back input value to its parent
@@ -29,42 +31,80 @@ class CategoryContainer extends Component {
 
     const answerInput = this.answerInput.current.value.toLowerCase()
     const answerExpected = this.state.category.clues[currentQuestionIndex].answer.toLowerCase();
+    const score = 0;
 
-    if(answerInput === answerExpected && currentQuestionIndex <= this.state.category.clues_count - 2 ){
+    if(answerInput === answerExpected ){
       console.log('good');
       currentQuestionIndex = currentQuestionIndex + 1;
       this.setState({
-        currentQuestion: currentQuestionIndex 
+        currentQuestion: currentQuestionIndex,
+        score: currentQuestionIndex
       })
       this.answerInput.current.value = ""
       console.log(currentQuestionIndex);
       console.log(this.state.currentQuestion);
+      console.log(this.state.category.clues_count);
     }
     else{
       console.log('bad');
       console.log(currentQuestionIndex);
+      this.setState(prevState => ({
+        lives: prevState.lives - 1
+      }));
+      console.log(this.state.lives);
     }
-    
-    
-    
-    // increment currentQuestion
-    // save in the storage the id of the question
-    // if no more question, remove category from categories playable
-    // increment score somewhere and redirect to /
-    // check if answer is equal to the requested answer from the current question
   }
+  handleScore = (e) => {
+    e.preventDefault();
+    console.log('uvgdjgv');
+  }
+  
 
   render() {
     const { category, currentQuestion } = this.state;
+    // const categoryLength = this.state.category.clues_count;
     // at first render, category will be null so we need to wait
     // before using data.
+    if (this.state.lives === 0) {
+      return (
+        <div>
+          <p>Game Over!</p>
+          <button type="button" onClick={this.resetCategory}>Reset category</button>
+        </div>
+      )
+    }
+    if(currentQuestion === 5){
+      return (
+        <div>
+          <p> iduuasdi</p>
+          <p>You win! You have {this.state.lives} lives left !</p>
+          <button type="button" onClick={this.resetCategory}>Reset category</button>
+        </div>
+      )
+    }
+    // console.log(categoryLength);
+    
+    
+    // if(currentQuestion === category){
+    //   return (
+    //     <div>
+    //       <p> iduuasdi</p>
+    //       {/* <p>You win! You have {this.state.lives} left !</p> */}
+    //       {/* <button type="button" onClick={this.resetCategory}>Reset category</button> */}
+    //     </div>
+    //   )
+    // }
+
     if (!category) return <div>is loading</div>
 
     return (
       <Category
         category={category}
+        score={this.state.score}
+        lives={this.state.lives}
         currentQuestionIndex={currentQuestion}
         handleSubmit={this.handleSubmit}
+        handleScore={this.handleScore}
         // plug createRef to chidlren
         answerInput={this.answerInput}
       />
